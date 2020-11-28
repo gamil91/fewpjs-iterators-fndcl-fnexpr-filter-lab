e7c170b687e84c62fc220fa8c87d616a296cc8d4
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-28 11:24:30 -0800

    filtered

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, string){
+    let newString = string.toLowerCase()
+    return drivers.filter(driver => driver.toLowerCase() === newString)
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driver => driver.startsWith(string))
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string)
+}
